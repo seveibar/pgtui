2603519de924c261aab81f6f48485e7962cc1850
Severin Ibarluzea <seveibar@gmail.com>
2021-11-14 01:17:33 -0800

    initial dockerize

--- lib/load-structure-sql.ts
+++ lib/load-structure-sql.ts
@@ -1,11 +1,28 @@
 import execa from "execa"
 import pgknexlove from "pgknexlove"
+import getDockerHost from "get-docker-host"
 
 export const loadStructureSQL = async (connectionInfo?: any) => {
-  const { host, port, user, password, database } = {
+  let { host, port, user, password, database } = {
     ...pgknexlove.default.getConnectionInfo(),
     ...connectionInfo,
   } as any
+
+  if (
+    process.env.RUNNING_IN_DOCKER &&
+    !process.env.POSTGRES_HOST &&
+    !process.env.DATABASE_URL
+  ) {
+    host = await new Promise((resolve, reject) => {
+      getDockerHost((err, ip) => {
+        if (err) return reject(err)
+        resolve(ip)
+      })
+    })
+    console.log(
+      `Detected we're in docker and no host has been set, using automatically detected host "${host}". Manually set POSTGRES_HOST or DATABASE_URL to prevent default from being overridden.`
+    )
+  }
 
   const result = await execa(
     "pg_dump",
