770bc6c5f10d25a96af95ddfcb076c0feb53c23f
Severin Ibarluzea <seveibar@gmail.com>
2022-04-11 09:21:10 -0700

    more progress

--- lib/tree-to-typescript-models/index.ts
+++ lib/tree-to-typescript-models/index.ts
@@ -8,17 +8,38 @@
  **/
 
 import { DatabaseTree } from "lib/types"
+import { Project, StructureKind } from "ts-morph"
 
 export const treeToTypescriptModels = (
   db: DatabaseTree
 ): { [filePath: string]: string } => {
   const fileTree = {}
+  const project = new Project({})
+
+  project.addSourceFilesAtPaths("db/types/**/*ts")
+  const indexFile = project.createSourceFile("db/types/index.ts", "")
+  indexFile.addImportDeclaration({
+    moduleSpecifier: "./knex",
+  })
+
+  for (const schemaName of Object.keys(db.schemas)) {
+    indexFile.addExportDeclaration({
+      moduleSpecifier: `./${schemaName}`,
+    })
+  }
+
+  console.log(indexFile.getText())
+
+  const knexFile = project.createSourceFile("db/types/knex.ts", "")
 
   for (const [schemaName, schema] of Object.entries(db.schemas)) {
     console.log({ schema })
   }
 
-  return {}
+  return {
+    "db/types/index.ts": "// typescript stuff",
+    "db/types/seam/Device.ts": "// typescript stuff",
+  }
 }
 
 export default treeToTypescriptModels
